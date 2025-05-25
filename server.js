8c09a25cb2c5ec527bcd6b7385131d3222be752e
Elad <elad.mor@wolt.com>
2025-05-25 17:46:19 +0300

    Initial commit for Slack server

--- server.js
+++ server.js
@@ -1,26 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-
-// Serve static index.html (optional)
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Slack events endpoint
-app.post('/slack/events', (req, res) => {
-  const { type, challenge } = req.body;
-
-  if (type === 'url_verification') {
-    return res.status(200).send(challenge);
-  }
-
-  res.status(200).send('Event received');
-});
-
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
